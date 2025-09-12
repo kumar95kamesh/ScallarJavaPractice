c128ffe8454698291ca83aeb451ea6cce7609049
AzureAD\RishabhMehta <rishabh@estater.com>
2025-09-13 00:35:06 +0530

    new changes

---
+++ src/day122_BackendLLDDesignPatternsDecoratorandFlyweightDesign/Client.java
@@ -0,0 +1,15 @@
+package Decorator;
+
+
+public class Client {
+    public static void main(String[] args) {
+        Beverage b = new DarkRoast();
+        System.out.println(b.getCost());
+        b = new Milk(b);
+        System.out.println(b.getCost());
+        b = new Milk(b);
+        System.out.println(b.getCost());
+        b = new Mocha(b);
+        System.out.println(b.getCost());
+    }
+}
